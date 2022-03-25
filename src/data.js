e5373ccc6587c4cc45fa107a8d9e4e399d46f27e
Arooba Siddiqi <aroobasiddiqi01@gmail.com>
2022-03-26 01:15:18 +0500

    ProductList & Product first commit

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export const products = [
+    {
+        id: 1,
+        img: "https://i.ibb.co/jL4FwvT/p1.png",
+        // href="https://ibb.co/JzkPpRK"
+        link: "https://github.com/AroobaSiddiqi/dr-kon",
+
+    },
+
+    {
+        id: 2,
+        img: "https://i.ibb.co/w6zR3Gn/p2.png",
+        // href="https://ibb.co/QmKfzSW"
+        link: "https://www.parkerpen.com/pen-collections.html",
+
+    },
+
+    {
+        id: 3,
+        img: "https://i.ibb.co/hRwzQkh/p3.png",
+        // href="https://ibb.co/C5DGCqN"
+        link: "https://github.com/AroobaSiddiqi/Arcadia",
+        // Ahmed Rihan's Design
+
+    },
+
+    {
+        id: 4,
+        img: "https://i.ibb.co/JKZwtZ8/p4.png",
+        // href="https://ibb.co/47kntkC"
+        link: "https://www.figma.com/proto/8aXvtvClHoIpQmo\
+        EfHI4Jg/FLORES-AMOR?node-id=115%3A923&starting-poi\
+        nt-node-id=115%3A923&scaling=scale-down",
+        // Rimsha Mirza's Design
+
+    },
+
+    {
+        id: 5,
+        img: "https://i.ibb.co/n6qbpb0/p5.png",
+        // href="https://ibb.co/HBLVZV2"
+        link: "https://www.figma.com/proto/NFYxKIcQHQww6bQ\
+        Wc4rWYt/SRE?node-id=36%3A19&scaling=scale-down&pag\
+        e-id=0%3A1&starting-point-node-id=36%3A19&show-pro\
+        to-sidebar=1",
+        // Ahmed Rihan & My Design
+
+    },
+
+    {
+        id: 6,
+        img: "https://i.ibb.co/mC5m6Rc/p6.png",
+        // href="https://ibb.co/7JtH2S1"
+        link: "https://www.apple.com/",
+
+    },
+
+];
